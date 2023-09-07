c97981242eacf0d81245f6ca159da3c163173079
Davide Cangiano <cangiano.0497@gmail.com>
2023-09-07 09:58:43 +0200

    Initial commit

---
+++ src/main/java/org/exercise/nations/Main.java
@@ -0,0 +1,12 @@
+package org.exercise.nations;
+
+public class Main {
+
+    private final static String url = "jdbc:mysql://localhost:3306/db-nations";
+    private final static String user = "root";
+    private final static String password = "root";
+
+    public static void main(String[] args) {
+
+    }
+}
